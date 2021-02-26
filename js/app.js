a12983b6fef49f3575a0bf7e24535f36125a7c76
hydrasyl <50514730+hydrasyl@users.noreply.github.com>
2021-02-26 20:39:11 +0100

    Création du panier + Ajout article

--- js/app.js
+++ js/app.js
@@ -8,30 +8,38 @@ fetch('http://localhost:3000/api/teddies').then(function (reponse) {
     }
 });
 
+let panier = [];
 let listProduit = document.getElementById('list-produits');
 
 function print(produits) {
     console.log(produits)
     for (let i in produits){
-        let produitConteneur = document.createElement('div');
-        produitConteneur.className = 'produit';
+        let produitConteneur        = document.createElement('div');
+        produitConteneur.className  = 'produit';
 
-        let btnConteneur = document.createElement('div');
+        let btnConteneur            = document.createElement('div');
 
-        let imgProduit      = document.createElement('img')
-        let produitTitre    = document.createElement('h3');
-        let produitPara     = document.createElement('p');
-        let produitPrix     = document.createElement('span');
-        let produitBouton   = document.createElement('button');
+        let imgProduit              = document.createElement('img')
+        let produitTitre            = document.createElement('h3');
+        let produitPara             = document.createElement('p');
+        let produitPrix             = document.createElement('span');
+        let produitBouton           = document.createElement('button');
+        let produitInput            = document.createElement('input')
 
-        imgProduit.src          = produits[i].imageUrl;
+        imgProduit.src              = produits[i].imageUrl;
 
-        produitBouton.className = 'btn btn-bleu';
+        produitBouton.className     = 'btn btn-bleu';
 
-        let textTitre   = document.createTextNode(produits[i].name);
-        let textPara    = document.createTextNode(produits[i].description);
-        let textPrix    = document.createTextNode(produits[i].price);
-        let textBoutton = document.createTextNode('Ajouter au panier');
+        produitBouton.onclick       = function (){ acheter(i, produits[i]); };
+
+        produitInput.id             = 'produit' + i;
+        produitInput.type           = 'number'
+        produitInput.min            = 1;
+
+        let textTitre               = document.createTextNode(produits[i].name);
+        let textPara                = document.createTextNode(produits[i].description);
+        let textPrix                = document.createTextNode(produits[i].price);
+        let textBoutton             = document.createTextNode('Ajouter au panier');
 
         produitTitre.appendChild(textTitre);
         produitPara.appendChild(textPara);
@@ -43,8 +51,23 @@ function print(produits) {
         produitConteneur.appendChild(imgProduit);
         produitConteneur.appendChild(produitTitre);
         produitConteneur.appendChild(produitPara);
+        produitConteneur.appendChild(produitInput);
         produitConteneur.appendChild(btnConteneur);
         btnConteneur.appendChild(produitPrix);
         btnConteneur.appendChild(produitBouton);
+    }
+}
+
+function acheter(id, produit) {
+    let quant       = parseInt(document.getElementById('produit' + id).value);
+
+    if (quant <= 0) { console.log('Quantité invalide'); return 1; }
+
+    if (panier[id]){
+        produit['quantite'] = produit['quantite'] + quant;
+        panier[id] = produit;
+    } else {
+        produit['quantite'] = quant;
+        panier[id] = produit;
     }
 }
